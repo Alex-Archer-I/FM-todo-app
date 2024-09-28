7378226dca41bf6fbdce53c2f6ae8f3021181b4f
Alex <archertrail@outlook.com>
2024-09-28 14:35:41 +0300

    new todo creating logic

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,11 +1,26 @@
-import {useState, useRef} from 'react';
+import {useState, useContext} from 'react';
+
+import {ToDoContext} from './store/context';
 
 const Form = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const {addTodo} = useContext(ToDoContext);
+
+    const changeHandler = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        addTodo(inputValue);
+        setInputValue('');
+    };
+
     return (
-        <form className="main__item to-do form">
+        <form className="main__item to-do form" onSubmit={submitHandler}>
             <div className="to-do__btn to-do__btn--round to-do__btn--check"></div>
-            <input type="text" className="to-do--input form__input" placeholder="Create a new todo..."/>
+            <input type="text" className="to-do--input form__input" placeholder="Create a new todo..." value={inputValue} onChange={changeHandler}/>
         </form>
     );
 };
